e86697fb3778b0c00e695d1f1545139a77c83298
Danniel <danniel.ang8@gmail.com>
2026-05-02 16:37:35 +0700

    Initial Commit

---
+++ bg-picker/bg_pickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  bg_pickerApp.swift
+//  bg-picker
+//
+//  Created by Danniel on 02/05/26.
+//
+
+import SwiftUI
+
+@main
+struct bg_pickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
